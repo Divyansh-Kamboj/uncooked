4e74f4d4a4064e0dd9ecf462f01a86c1f0ab97e8
Divyansh-Kamboj <dkamboj1301@gmail.com>
2025-08-13 18:51:00 +0530

    hosting stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,20 +3,24 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/uncooked/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: mode !== "production",
   },
 }));
